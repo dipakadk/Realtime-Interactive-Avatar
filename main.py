e360bcfaf374a31e7012ee0627ed6c5a3166c7a5
Dipak Adhikari <adhikarydeepak20@gmail.com>
2025-09-16 10:47:43 +0545

    frontend and fastapi setup

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from realtime-interactive-avatar!")
+import os
+import json
+import threading
+from fastapi import FastAPI, WebSocket, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse, HTMLResponse
+from dotenv import load_dotenv
+load_dotenv()
+
+DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+if not DEEPGRAM_API_KEY:
+    raise RuntimeError("Please set DEEPGRAM_API_KEY in .env")
+
+HEYGEN_API_KEY = os.getenv("HEYGEN_API_KEY")
+if not HEYGEN_API_KEY:
+    raise RuntimeError("Please set HEYGEN_API_KEY in .env")
+
+HEYGEN_SERVER_URL = os.getenv("HEYGEN_SERVER_URL")
+if not HEYGEN_API_KEY:
+    raise RuntimeError("Please set HEYGEN_SERVER_URL as https://api.heygen.com in .env")
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+templates = Jinja2Templates(directory="frontend/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
